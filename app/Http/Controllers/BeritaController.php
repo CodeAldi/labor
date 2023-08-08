2c510399211d5ddb8ec046c31111931bbaca19cd
Aldi Rizal Chan <codealdi@gmail.com>
2023-08-09 01:47:25 +0700

    view dan func index pada berita

--- app/Http/Controllers/BeritaController.php
+++ app/Http/Controllers/BeritaController.php
@@ -12,7 +12,7 @@ class BeritaController extends Controller
      */
     public function index()
     {
-        //
+        return view('backend.admin.berita.index');
     }
 
     /**
---
+++ resources/views/backend/admin/berita/index.blade.php
@@ -0,0 +1,12 @@
+@extends('layouts.backend.main')
+
+@section('content')
+    <div class="content-wrapper">
+        <section class="content-header">
+
+        </section>
+        <section class="content">
+            
+        </section>
+    </div>
+@endsection
