f81116a039aceae079cb7efc5e66b2b7d0c51f82
aldi-computer-home <codealdi@gmail.com>
2023-09-05 15:59:19 +0700

    menambahkan gambar struktur organisasi

--- resources/views/front/profile/struktur.blade.php
+++ resources/views/front/profile/struktur.blade.php
@@ -3,9 +3,8 @@
 @section('content')
     <main>
         <div class="container mt-5">
-            <h1>Judul</h1>
-            <p>deskripsi</p>
-            <img src="{{ asset('images/dummy_600x400.png') }}" class="w-100" alt="">
+            <h1>Stuktur Organisasi</h1>
+            <img src="{{ asset('images/struktur-organisasi.png') }}" class="w-100" alt="">
         </div>
     </main>
 @endsection
