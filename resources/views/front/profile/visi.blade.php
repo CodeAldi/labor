4ab823f6345f663d3ab70c7c71cc40dd7595718a
aldi-computer-home <codealdi@gmail.com>
2023-09-05 16:41:32 +0700

    mengisi teks visi

--- resources/views/front/profile/visi.blade.php
+++ resources/views/front/profile/visi.blade.php
@@ -3,27 +3,49 @@
 @section('content')
 <main>
     <div class="container py-5">
-        <h1>Judul</h1>
-        <p>deskripsi</p>
-        <img src="{{ asset('images/dummy_600x400.png') }}" width="100%" height="400px" alt="">
-        <h1>Judul</h1>
-        <p>deskripsi</p>
-        <p class="" style="text-align: justify; text-justify: inter-word">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Repellat dolores debitis deleniti
-            voluptatum. Suscipit dolorum tempore qui, magnam sequi quae amet accusantium beatae unde commodi, doloribus
-            quisquam, tempora excepturi libero inventore. Tempore quisquam impedit, molestias reiciendis quibusdam animi
-            rerum necessitatibus ullam recusandae fugit natus quaerat repellat at officia laboriosam dolor.</p>
-        <h1>Judul</h1>
-        <p>deskripsi</p>
-        <p class="" style="text-align: justify; text-justify: inter-word">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Repellat dolores debitis deleniti
-            voluptatum. Suscipit dolorum tempore qui, magnam sequi quae amet accusantium beatae unde commodi, doloribus
-            quisquam, tempora excepturi libero inventore. Tempore quisquam impedit, molestias reiciendis quibusdam animi
-            rerum necessitatibus ullam recusandae fugit natus quaerat repellat at officia laboriosam dolor.</p>
-        <h1>Judul</h1>
-        <p>deskripsi</p>
-        <p class="" style="text-align: justify; text-justify: inter-word">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Repellat dolores debitis deleniti
-            voluptatum. Suscipit dolorum tempore qui, magnam sequi quae amet accusantium beatae unde commodi, doloribus
-            quisquam, tempora excepturi libero inventore. Tempore quisquam impedit, molestias reiciendis quibusdam animi
-            rerum necessitatibus ullam recusandae fugit natus quaerat repellat at officia laboriosam dolor.</p>
+        <h1 class="text-decoration-underline">Visi</h1>
+        <p class="" style="text-align: justify; text-justify: inter-word">
+            <q>Menjadi Laboratorium Terpadu yang terkemuka, terakreditasi, dan berstandar internasional dalam melayani penelitian, pengujian dan pelatihan untuk mendukung Universitas Negeri Padang sebagai universitas riset yang unggul
+            </q>
+        </p>
+
+
+        <h1 class="text-decoration-underline">Misi</h1>
+        <p class="" style="text-align: justify; text-justify: inter-word">
+        <ol>
+            <li>Menyediakan sarana dan prasarana untuk kegiatan penelitian,
+            pengembangan, inovasi dalam bisnis sains, teknologi, dan
+            sosial humaniora untuk civitas academica Universitas Negeri
+            Padang, instansi/lembaga penelitian, masyarakat dan industri.</li>
+            <li>Menyediakan layanan penelitian dan pengembangan yang
+            berkelanjutan dalam bidang sains, teknologi, sosial
+            humaniora untuk civitas academica Universitas Negeri Padang,
+            instansi/lembaga penelitian, masyarakat dan industri.</li>
+            <li>Menyediakan sumber daya penelitian yang berkompeten dan
+            Berkualitas</li>
+            <li>Memberikan layanan pengujian/analisis laboratorium,
+            sertifikasi, kalibrasi yang dapat dipertanggungjawabkan
+            keakuratan dan keabsahan hasil kegiatannya.</li>
+            Memberikan layanan kerjasama bidang penelitian, pelatihan,
+            pengujian/analisis laboratorium dengan instansi dan industri
+            baik dalam negeri maupun luar negeri.
+        </ol>
+        </p>
+        <h1 class="text-decoration-underline">Tujuan</h1>
+        <p class="" style="text-align: justify; text-justify: inter-word">
+        <ol>
+            <li>Memberikan layanan barang dan jasa yang berkualitas, transparan,
+            akuntabel dan profesional di tingkat nasional dan internasional.</li>
+            <li>Menghasilkan labor pengujian yang terpercaya sesuai dengan standar SNI ISO/IEC 17025:2017</li>
+            <li>Menghasilkan standar informasi seluruh aset laboratorium di Universitas Negeri Padang</li>
+            <li>Meningkatkan kuantitas dan kualitas kerjasama yang produktif dan
+            bereputasi global</li>
+            <li>Mewujudkan tata kelola Laboratorium Terpadu yang transparan,
+            akuntabel, bertanggung jawab dan adil.</li>
+            <li>Menghasilkan labor-labor yang unggul melalui program akreditasi/
+            sertifikasi peralatan laboratorium di tingkat nasional dan internasional.</li>
+        </ol>
+        </p>
     </div>
 </main>
 @endsection
