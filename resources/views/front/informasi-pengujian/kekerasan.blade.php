15931eb97806273b5390f00b4641e4e027539120
aldi-computer-home <codealdi@gmail.com>
2023-09-05 19:32:33 +0700

    mengisi gambar pengujian kekerasan bahan

--- resources/views/front/informasi-pengujian/kekerasan.blade.php
+++ resources/views/front/informasi-pengujian/kekerasan.blade.php
@@ -1,17 +1,7 @@
 @extends('layouts.front.main') @section('content')
 <main>
     <div class="container pt-5">
-        <h1>Judul</h1>
-        <p>Deskripsi</p>
-        <img src="{{ asset('images/dummy_600x400.png') }}" class="w-100" alt="" />
-        <p class="py-2" style="text-align: justify; text-justify: inter-word">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Facere
-            sapiente debitis voluptatem eos aperiam? Numquam tenetur saepe harum
-            explicabo? Cupiditate laudantium quia eum deserunt dicta ut ratione
-            perspiciatis provident ullam consequatur beatae, nobis deleniti
-            atque dolorem harum nisi, ab labore, quam repudiandae laborum
-            adipisci. Laborum animi ipsa eos error praesentium!
-        </p>
+        <img src="{{ asset('images/informasi-pengujian/kekerasan bahan/thumbnail.png') }}" class="w-100" alt="" />
         <!-- sub judul 1 -->
         <div class="row">
             <div class="col">
@@ -20,7 +10,7 @@
         </div>
         <div class="row">
             <div class="col-4">
-                <img src="{{ asset('images/dummy_600x400.png') }}" width="100%" alt="" />
+                <img src="{{ asset('images/informasi-pengujian/kekerasan bahan/ver1.png') }}" width="100%" alt="" />
             </div>
             <div class="col-8">
                 <p class="" style="text-align: justify; text-justify: inter-word">
@@ -42,7 +32,7 @@
         </div>
         <div class="row">
             <div class="col-4">
-                <img src="{{ asset('images/dummy_600x400.png') }}" width="100%" alt="" />
+                <img src="{{ asset('images/informasi-pengujian/kekerasan bahan/ver2.png') }}" width="100%" alt="" />
             </div>
             <div class="col-8">
                 <p class="" style="text-align: justify; text-justify: inter-word">
@@ -64,7 +54,7 @@
         </div>
         <div class="row">
             <div class="col-4">
-                <img src="{{ asset('images/dummy_600x400.png') }}" width="100%" alt="" />
+                <img src="{{ asset('images/informasi-pengujian/kekerasan bahan/img (1).jpg') }}" width="100%" alt="" />
             </div>
             <div class="col-8">
                 <p class="" style="text-align: justify; text-justify: inter-word">
@@ -80,16 +70,16 @@
         </div>
         <div class="row mt-3">
             <div class="col-3">
-                <img src="{{ asset('images/dummy_600x400.png') }}" width="100%" alt="">
+                <img src="{{ asset('images/informasi-pengujian/kekerasan bahan/img (2).jpg') }}" width="100%" alt="">
             </div>
             <div class="col-3">
-                <img src="{{ asset('images/dummy_600x400.png') }}" width="100%" alt="">
+                <img src="{{ asset('images/informasi-pengujian/kekerasan bahan/img (3).jpg') }}" width="100%" alt="">
             </div>
             <div class="col-3">
-                <img src="{{ asset('images/dummy_600x400.png') }}" width="100%" alt="">
+                <img src="{{ asset('images/informasi-pengujian/kekerasan bahan/img (4).jpg') }}" width="100%" alt="">
             </div>
             <div class="col-3">
-                <img src="{{ asset('images/dummy_600x400.png') }}" width="100%" alt="">
+                <img src="{{ asset('images/informasi-pengujian/kekerasan bahan/img (5).jpg') }}" width="100%" alt="">
             </div>
         </div>
     </div>
