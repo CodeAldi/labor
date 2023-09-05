f0d5592b822a506ed836627ee7128a2b2a5c6e8f
aldi-computer-home <codealdi@gmail.com>
2023-09-05 15:38:51 +0700

    mengisi teks sejarah singkat

--- resources/views/front/profile/sejarah.blade.php
+++ resources/views/front/profile/sejarah.blade.php
@@ -9,20 +9,25 @@
                 <p>deskripsi</p>
                 <article>
                     <img src="{{ asset('images/dummy_600x400.png') }}" width="100%" alt="">
-                    <p class="pt-2" style="text-align: justify; text-justify: inter-word">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nulla, saepe? Lorem ipsum
-                        dolor sit amet consectetur adipisicing elit. Minus pariatur ducimus, nihil voluptatum numquam,
-                        debitis nobis commodi itaque natus exercitationem sit blanditiis. Voluptatum maiores dolorum
-                        tempora est ducimus voluptate quasi.</p>
-                    <p class="pt-2" style="text-align: justify; text-justify: inter-word">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Qui labore rerum inventore
-                        aspernatur laborum accusantium harum quos iure exercitationem tenetur excepturi temporibus sit
-                        minima dolores, ipsum accusamus! Vitae vel numquam quas autem excepturi porro ratione est eum
-                        placeat veniam atque voluptas, dicta ea consequatur nihil perspiciatis deserunt facere illum
-                        non.</p>
-                    <p class="pt-2" style="text-align: justify; text-justify: inter-word">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Qui labore rerum inventore
-                        aspernatur laborum accusantium harum quos iure exercitationem tenetur excepturi temporibus sit
-                        minima dolores, ipsum accusamus! Vitae vel numquam quas autem excepturi porro ratione est eum
-                        placeat veniam atque voluptas, dicta ea consequatur nihil perspiciatis deserunt facere illum
-                        non.</p>
+                    <p class="pt-2" style="text-align: justify; text-justify: inter-word">
+                    Laboratorium Terpadu Universitas Negeri Padang merupakan salah satu divisi pada Pusat Pengembangan Laboratorium Terpadu
+                    di lingkungan UNP yang berdiri berdasarkan Surat Keputusan Rektor No. 3509 Tahun 2017 tentang Standar Pelayanan Publik
+                    Universitas Negeri Padang, dan Surat Keputusan Rektor No. 143 Tahun 2021 tentang Penunjukkan Tim Pengelola Laboratorium
+                    Terpadu Universitas Negeri Padang.<br/><br/>Laboratorium Terpadu dipimpin oleh seorang Kepala Pusat Pengembangan Laboratorium
+                    Terpadu dan bertanggung jawab kepada Rektor melalui Penanggungjawab sekaligus Ketua Lembaga Pengembangan Pembelajaran
+                    dan Penjaminan Mutu (LP3M). Laboratorium Terpadu Universitas Negeri Padang mempunyai tugas melaksanakan layanan
+                    laboratorium untuk kegiatan pengujian dan kalibrasi, pendidikan, penelitian dan pengabdian kepada masyarakat.
+                    Panduan Mutu Laboratorium Terpadu Universitas Negeri Padang disusun berdasarkan ISO/IEC 17025:2017 mengenai Persyaratan
+                    Umum Kompetensi Laboratorium Pengujian dan Kalibrasi sesuai dengan ruang lingkup laboratorium. Panduan Mutu ini dibuat
+                    dengan tujuan agar digunakan sebagai:
+                    <ol>
+                        <li>Acuan dalam penerapan sistem manajemen berdasarkan ISO/IEC 17025:2017.</li>
+                        <li>Acuan dalam pelaksanaan audit implementasi sistem manajemen baik audit internal maupun audit yang dilakukan oleh pihak
+                        ketiga (audit eksternal).</li>
+                        <li>Acuan dalam upaya peningkatan dan pemeliharaan kinerja.</li>
+                        <li>Bukti telah menerapkan Sistem Manajemen Mutu laboratorium berdasarkan ISO/IEC 17025:2017</li>
+                    </ol>
+                    </p>
                 </article>
             </div>
             <div class="col-3">
