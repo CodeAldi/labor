c93c2ac2c7194a795286b9958d8a15eb7dbcf052
Aldi Rizal Chan <codealdi@gmail.com>
2023-09-28 10:16:34 +0700

    icon tab

--- resources/views/layouts/front/main.blade.php
+++ resources/views/layouts/front/main.blade.php
@@ -4,7 +4,8 @@
 <head>
     <meta charset="utf-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1" />
-    <title>Labdu</title>
+    <title>{{ ENV('app_name') }}</title>
+    <link rel="icon" type="image/x-icon" href="{{ asset('images/LOGO UNIVERSITAS NEGERI PADANG.png') }}">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-4bw+/aepP/YC94hEpVNVgiZdgIC5+VKNBQNGCHeKRQN+PtmoHDEXuppvnDJzQIu9" crossorigin="anonymous" />
     <link rel="stylesheet" href="{{ asset('css/style.css') }}" />
